ab0e4f108f7c159ed824d4bfe8007eceacefe2d3
Peter Boersma <peter@growingminds.nl>
2024-11-11 21:47:07 +0100

    Added basic move controls to player

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import player
 
 def main():
     pygame.init()
-    display = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+
+
+    pl = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updateable.add(pl)
+    drawable.add(pl)
 
     while True:
         for event in pygame.event.get():
@@ -11,8 +24,16 @@ def main():
                 return
 
         display.fill((0, 0, 0))
+
+        for entity in updateable:
+            entity.update(dt)
+
+        for entity in drawable:
+            entity.draw(display)
         
         pygame.display.update()
+        
+        dt = gameClock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
